f5bbb651921a6ab7bda4a3cc0f1ca2547c17b607
shipl12 <ziadshipl00@gmail.com>
2025-06-06 23:53:37 +0300

    Add .next/static/chunks/src_app_wiring_page_tsx_a993527f._.js 1749243218423

---
+++ .next/static/chunks/src_app_wiring_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_a56cc5cd._.js",
+  "static/chunks/src_2dba9f05._.js"
+],
+    source: "dynamic"
+});
