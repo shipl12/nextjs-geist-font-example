c6dd897c1f96aa0ebe0a5d6d67898e89f3c768e8
shipl12 <ziadshipl00@gmail.com>
2025-06-06 23:53:14 +0300

    Add .next/static/chunks/src_app_components_page_tsx_a993527f._.js 1749243196307

---
+++ .next/static/chunks/src_app_components_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d0122cc6._.js",
+  "static/chunks/src_f4f725b7._.js"
+],
+    source: "dynamic"
+});
