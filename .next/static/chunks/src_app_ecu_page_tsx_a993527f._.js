3cdeb1f9ba10426f03918ba28a470276a00a5521
shipl12 <ziadshipl00@gmail.com>
2025-06-06 23:52:56 +0300

    Add .next/static/chunks/src_app_ecu_page_tsx_a993527f._.js 1749243178170

---
+++ .next/static/chunks/src_app_ecu_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_d7574da4._.js",
+  "static/chunks/src_7cbd583a._.js"
+],
+    source: "dynamic"
+});
